0e1e72bd88fd96cf567961220b590e2fdfc92e68
Vinayak <80584437+Vinayak2835@users.noreply.github.com>
2021-07-12 15:53:47 +0530

    [Vinayak] Add: StudentDAO

--- src/main/java/com/ResultSetExtractor/StudentDAO.java
+++ src/main/java/com/ResultSetExtractor/StudentDAO.java
@@ -1,27 +1,13 @@
 package com.ResultSetExtractor;
 
 import java.util.List;
-
-
-
 import com.DTO.StudentDTO;
-
 
 public interface StudentDAO {
 	
-	
-	
-	List<StudentDTO>  selectAll();
-	
+        List<StudentDTO>  selectAll();
 	void insert(StudentDTO  student);
-	
 	StudentDTO update(int id);
-	
 	void updateStudentInformation(StudentDTO  studentDTO);
-	
-	
 	public void delete(int id);
-	
-	
-
 }
