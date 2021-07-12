c60dfb0c1c30d959bf1205f87fb185d24cb240ff
Vinayak <80584437+Vinayak2835@users.noreply.github.com>
2021-07-12 15:58:02 +0530

    [Vinayak] Add: StudentService

--- src/main/java/com/StudentService/StudentService.java
+++ src/main/java/com/StudentService/StudentService.java
@@ -1,19 +1,13 @@
 package com.StudentService;
 
 import java.util.List;
-
 import com.DTO.StudentDTO;
 
 public interface StudentService {
 	
-	
 	List<StudentDTO>  selectAll();
 	void insert(StudentDTO  student);
-	
 	StudentDTO update(int id);
-	
 	void updateStudentInformation(StudentDTO  studentDTO);
-	
 	public void delete(int id);
-
 }
